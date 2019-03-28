f06aebe348c4e4b33445ce64c01bc49b6e2ddafd
Matthew Aguirre <matthewaguirre@JSA-190134.local>
2019-03-28 13:53:50 -0700

    retrieves data file from bucket

--- S3Retriever/s3.js
+++ S3Retriever/s3.js
@@ -4,14 +4,6 @@ AWS.config.update({region: process.env.aws_region})
 
 var credentials = process.env;
 
-//`s3://jumpshot-data-samples/insights-stable-v2-20190302/`
-
-//`https://console.aws.amazon.com/s3/buckets/jumpshot-data-samples/insights-stable-v2-20190302/?region=us-west-2&tab=overview`
-
-//s3://jumpshot-data-samples/
-
-//insights-stable-v2-20190302
-
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
@@ -24,7 +16,7 @@ var bucketParams = {
 // Call S3 to obtain a list of the objects in the bucket
 s3.listObjects(bucketParams, function(err, data) {
   if (err) {
-    console.log("Error", err);
+    console.log("Error ", err);
   } else {
     console.log("Success", data);
   }
@@ -32,8 +24,15 @@ s3.listObjects(bucketParams, function(err, data) {
 
 var file = s3.getObject(
   { Bucket: 'jumpshot-data-samples',
-  Prefix: 'insights-stable-v2-20190302'
-}, function(error, data) {
-  
-}
-)
+    Key: 'insights-stable-v2-20190302/1800flowers.com/2014/01/02/part-00000-2551eefb-78be-409d-a9f8-347e791cc0f5.c000.txt.gz'
+}, function(err, data) {
+    if (err) {
+      console.log("Error ", err)
+    } else {
+      console.log('Data Retrieved: ')
+      console.log(data)
+      var objectData = data.Body.toString('utf-8');
+      console.log(objectData);
+    }
+  }
+);
