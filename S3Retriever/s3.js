d488bb71457a99d6047d8a9450faa1f81322cd5d
Matthew Aguirre <matthewaguirre@JSA-190134.local>
2019-04-18 15:56:16 -0700

    adds hardcoded testing data

--- S3Retriever/s3.js
+++ S3Retriever/s3.js
@@ -15,8 +15,6 @@ var bucketParams = {
   Prefix: 'insights-stable-v2-20190302'
 };
 
-
-
 var dataOutput = [];
 var nCols = 0;
 
@@ -72,22 +70,40 @@ module.exports = {
   getS3FileList: function(s3) {
     // Call S3 to obtain a list of the objects in the bucket
     var records = [];
-    s3.listObjects(bucketParams, function(err, data) {
-      if (err) {
-        console.log("Error ", err);
-      } else {
-        //console.log("Success", data);
-        //temoporarily limited list of records - TODO remove later
-        for (var i = 0; i < 3; i++) {
-          records.push(data.Contents[i]);
-          console.log(records);
-        }
-      }
-    });
+    // s3.listObjects(bucketParams, function(err, data) {
+    //   if (err) {
+    //     console.log("Error ", err);
+    //   } else {
+    //     //console.log("Success", data);
+    //     //temoporarily limited list of records - TODO remove later
+    //     for (var i = 0; i < 3; i++) {
+    //       records.push(data.Contents[i]);
+    //     }
+    //     console.log(records);
+    //   }
+    // });
+    //TEMP HARD CODED DATA - REMOVE LATER
+    // NOTE: objects have had Etag param removed
+    records = [ { Key:
+     'insights-stable-v2-20190302/1800flowers.com/2014/01/01/_SUCCESS',
+    LastModified: '2019-03-20T23:15:30.000Z',
+    Size: 0,
+    StorageClass: 'STANDARD' },
+  { Key:
+     'insights-stable-v2-20190302/1800flowers.com/2014/01/01/part-00000-2551eefb-78be-409d-a9f8-347e791cc0f5.c000.txt.gz',
+    LastModified: '2019-03-03T16:01:09.000Z',
+    Size: 2624,
+    StorageClass: 'STANDARD' },
+  { Key:
+     'insights-stable-v2-20190302/1800flowers.com/2014/01/01/part-00001-2551eefb-78be-409d-a9f8-347e791cc0f5.c000.txt.gz',
+    LastModified: '2019-03-03T16:01:09.000Z',
+    Size: 2516,
+    StorageClass: 'STANDARD' } ]
+
     return records;
   }
 
 };
 
 var records = module.exports.getS3FileList(s3);
-//console.log(records);
+console.log(records);
