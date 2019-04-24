a930067f35f14e8bee36c6637fc70f59b3649349
Matthew Aguirre <mpaguirre@dons.usfca.edu>
2019-04-24 14:59:50 -0700

    adds html structure building pseudoclass

--- wdc/js/dataFileStructure.js
+++ wdc/js/dataFileStructure.js
@@ -1,8 +1,31 @@
 
+module.exports  = {
 
-export default class Structure {
-  constructor() {
+Structure: function (fileInfo) {
+
+    var key = fileInfo.Key.split('/');
+
+    this.name = key[1];
+    this.part = key[5].split('-')[1];
+    this.lastModified = fileInfo.LastModified.split('T')[0];
+
+    return (
+            `<div>
+            <label>"Name: ${this.name}"</label>
+            <label>"Part: ${this.part}"</label>
+            <label>"Date: ${this.lastModified}"</label>
+            </div>`
+    )
 
   }
-  
 }
+//
+// var data = { Key:
+//    'insights-stable-v2-20190302/1800flowers.com/2014/01/01/part-00000-2551eefb-78be-409d-a9f8-347e791cc0f5.c000.txt.gz',
+//   LastModified: '2019-03-03T16:01:09.000Z',
+//   Size: 2624,
+//   StorageClass: 'STANDARD' }
+//
+// var s = module.exports.Structure(data);
+//
+// console.log(s);
