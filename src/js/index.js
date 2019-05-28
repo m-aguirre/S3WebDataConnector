c43ffd93088df5717b3bb04c34b68db76630dfa9
Matthew Aguirre <mpaguirre@dons.usfca.edu>
2019-05-28 15:56:55 -0700

    moves hardcoded data

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,42 @@ var structureBuilder = require('./dataFileStructure.js');
 var dataFromS3 = ['empty'];
 
 
+var dataAsJson = [ { id: 0,
+start: 'Feb 25, 2019',
+conversions: 15172,
+end: 'Feb 25, 2019',
+percent: 1 },
+{ id: 1,
+start: 'Feb 26, 2019',
+conversions: 10990,
+end: 'Feb 26, 2019',
+percent: 1 },
+{ id: 2,
+start: 'Feb 27, 2019',
+conversions: 9824,
+end: 'Feb 27, 2019',
+percent: 1 },
+{ id: 3,
+start: 'Feb 28, 2019',
+conversions: 10454,
+end: 'Feb 28, 2019',
+percent: 1 },
+{ id: 4,
+start: 'Mar 1, 2019',
+conversions: 12698,
+end: 'Mar 1, 2019',
+percent: 1 },
+{ id: 5,
+start: 'Mar 2, 2019',
+conversions: 12191,
+end: 'Mar 2, 2019',
+percent: 1 },
+{ id: 6,
+start: 'Mar 3, 2019',
+conversions: 13483,
+end: 'Mar 3, 2019',
+percent: 1 } ];
+
 (function () {
   var fileNames = ["exampleJanuary.tsv", "exampleFebruary.tsv"];
 
@@ -17,70 +53,35 @@ var dataFromS3 = ['empty'];
 
   myConnector.getSchema = function(schemaCallback) {
 
-    // var cols = []
-    //   for (let i = 0; i < 19; i++) {
-    //     let obj = {
-    //       id: i.toString(),
-    //       dataType: tableau.dataTypeEnum.string
-    //     };
-    //     cols.push(obj)
-    //   }
-    var dataAsJson = [ { id: 0,
- start: 'Feb 25, 2019',
- conversions: 15172,
- end: 'Feb 25, 2019',
- percent: 1 },
-{ id: 1,
- start: 'Feb 26, 2019',
- conversions: 10990,
- end: 'Feb 26, 2019',
- percent: 1 },
-{ id: 2,
- start: 'Feb 27, 2019',
- conversions: 9824,
- end: 'Feb 27, 2019',
- percent: 1 },
-{ id: 3,
- start: 'Feb 28, 2019',
- conversions: 10454,
- end: 'Feb 28, 2019',
- percent: 1 },
-{ id: 4,
- start: 'Mar 1, 2019',
- conversions: 12698,
- end: 'Mar 1, 2019',
- percent: 1 },
-{ id: 5,
- start: 'Mar 2, 2019',
- conversions: 12191,
- end: 'Mar 2, 2019',
- percent: 1 },
-{ id: 6,
- start: 'Mar 3, 2019',
- conversions: 13483,
- end: 'Mar 3, 2019',
- percent: 1 } ];
+    var cols = []
+      for (let i = 0; i < 19; i++) {
+        let obj = {
+          id: i.toString(),
+          dataType: tableau.dataTypeEnum.string
+        };
+        cols.push(obj)
+      }
 
-    var cols = [{
-      id: 'id',
-      dataType: tableau.dataTypeEnum.string
-    }, {
-      id: 'start',
-      alias: 'start-date',
-      dataType: tableau.dataTypeEnum.string
-    }, {
-      id: 'conversions',
-      alias: 'conversions',
-      dataType: tableau.dataTypeEnum.string
-    }, {
-      id: 'end',
-      alias: 'end-date',
-      dataType: tableau.dataTypeEnum.string
-    } , {
-      id: 'percent',
-      alias: 'percentage',
-      dataType: tableau.dataTypeEnum.string
-    }];
+    // var cols = [{
+    //   id: 'id',
+    //   dataType: tableau.dataTypeEnum.string
+    // }, {
+    //   id: 'start',
+    //   alias: 'start-date',
+    //   dataType: tableau.dataTypeEnum.string
+    // }, {
+    //   id: 'conversions',
+    //   alias: 'conversions',
+    //   dataType: tableau.dataTypeEnum.string
+    // }, {
+    //   id: 'end',
+    //   alias: 'end-date',
+    //   dataType: tableau.dataTypeEnum.string
+    // } , {
+    //   id: 'percent',
+    //   alias: 'percentage',
+    //   dataType: tableau.dataTypeEnum.string
+    // }];
 
     var tableSchema = {
       id: 'feedPrototype',
@@ -126,7 +127,7 @@ const formatData = (data) => {
 
 //TODO add catch for when zero elements are checked
 $(document).ready(function () {
-  console.log('v9')
+  console.log('v10')
   var fileNameToRequest = [];
     $("#submitButton").click(function () {
       $('.file-info-tile').each(function(index) {
