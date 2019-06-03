15ea307f6924b22c9129f088e902c39f07f6550c
Matthew Aguirre <mpaguirre@dons.usfca.edu>
2019-06-03 14:34:17 -0700

    test full file reading

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,7 @@ var structureBuilder = require('./dataFileStructure.js');
 var dataFromS3 = ['empty'];
 
 var feedJson = [ {
-"0": "20140101",
+"0": "TEST",
 "1": "222253",
 "2": "57fbde4915cdba5fad8997d297b44fd6",
 "3": "1800flowers.com",
@@ -18,13 +18,13 @@ var feedJson = [ {
 id: 0},
 {
 "0": "20140101",
-"1": "072541",
+"1": "TEST",
 "2": "2ff4f3883bf39c55989499e291f948c8",
 "3": "1800flowers.com",
 "4": "Product page",
  id: 1 },
 {
-"0": "20140101",
+"0": "TEST",
 "1": "144128",
 "2": "4a1f123060def68997e3266cbbd4888a",
 "3": "1800flowers.com",
@@ -33,7 +33,7 @@ id: 2 },
 {
 "0": "20140101",
 "1": "220710",
-"2": "2afd4bd95c09afeba81ff2db6003dbc1",
+"2": "TEST",
 "3": "1800flowers.com",
 '4': "Product page" ,
 id: 3}];
@@ -62,24 +62,7 @@ id: 3}];
 // console.log(feedJson);
 // console.log(cols);
 
-// cols = []
-// cols.push({
-//   id: "id",
-//   dataType: tableau.dataTypeEnum.string
-// });
-// for (let i = 0; i < 5; i++) {
-//   let obj = {
-//     id: i.toString(),
-//     dataType: tableau.dataTypeEnum.string
-//   };
-//   cols.push(obj)
-// }
-// console.log(cols);
-
-
 (function () {
-
-
   var myConnector = tableau.makeConnector();
 
   // var cols = [{
@@ -169,10 +152,10 @@ const formatData = (data) => {
   let dataCollection = []
   for (let i = 0; i < data.length ; i++) { // data.length 4
     let obj = {id: i}
+    dataCollection.push(obj);
     for (let j = 0; j < data[i].length; j++) { //data[i].length 5
       obj[j.toString()] = data[i][j]
     }
-    dataCollection.push(obj);
   }
   return dataCollection
 }
@@ -206,7 +189,6 @@ $(document).ready(function () {
           console.log('Response Data: ')
           var formattedData = formatData(res.data);
           feedJson = formattedData;
-          console.log(formattedData);
           console.log(feedJson);
 
           tableau.connectionName = "Jumpshot Dynamic Feed";
