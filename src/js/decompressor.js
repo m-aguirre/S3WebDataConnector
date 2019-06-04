9e9d246119de41637fadb61edf123e9f1ffc2822
Matthew Aguirre <mpaguirre@dons.usfca.edu>
2019-06-04 14:44:35 -0700

    removes comment blocks from decompressor

--- src/js/decompressor.js
+++ src/js/decompressor.js
@@ -1,33 +1,5 @@
 var zlib = require('browserify-zlib');
 
-
-//
-// zlib.gunzip(data.Body, function (err, result) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             var schema = module.exports.generateSchema();
-//             dataOutput.push(schema);
-//             var dataArray = result.toString('utf8').split('\n');
-//             for (i = 0; i < dataArray.length; i++) {
-//               dataArray[i] = dataArray[i].split('\u0001');
-//               //console.log(dataArray[i]);
-//               if (nCols === 0) {
-//                 nCols = dataArray[i].length;
-//               }
-//               if (dataArray[i][0] === '') {
-//                 continue;
-//               } else {
-//                 dataOutput.push(dataArray[i]);
-//               }
-//             }
-//             //console.log(dataArray);
-//         }
-//         // for (i = 0; i < dataOutput.length; i++) {
-//         //   console.log(dataOutput[i]);
-//         // }
-// });
-
 class Decompressor {
   constructor(data) {
     this.data = data
@@ -53,11 +25,7 @@ class Decompressor {
                     this.unzippedFile.push(dataArray[i]);
                   }
                 }
-                //console.log(dataArray);
             }
-            // for (i = 0; i < dataOutput.length; i++) {
-            //   console.log(dataOutput[i]);
-            // }
     });
     return this.unzippedFile;
   }
