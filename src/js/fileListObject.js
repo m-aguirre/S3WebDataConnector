5e41c8fe0947a986e0e17a56b542ff1092bf32a9
Matthew Aguirre <mpaguirre@dons.usfca.edu>
2019-06-12 15:19:55 -0700

    adds refactored fileListObject

---
+++ src/js/fileListObject.js
@@ -0,0 +1,32 @@
+
+class FileListObject {
+  constructor(file) {
+    let key = file.Key.split('/');
+    this.fileKey = file.Key;
+    this.name = key[1];
+    this.dateOfFile = key[2] + '-' + key[3] + '-' + key[4];
+    this.part = key[5].split('-')[1];
+    this.lastModified = file.LastModified.split('T')[0];
+  }
+
+  makeFileObject() {
+    const obj = `
+    <div class='file-list-item'>
+    <div class="checkbox-container">
+      <input type="checkbox" name="${this.fileKey}">
+    </div>
+      <div class='file-info-container'>
+          <div class='file-info-element'>${this.name}</div>
+          <div class='file-info-element'>${this.dateOfFile}</div>
+          <div class='file-info-element'>${this.part}</div>
+          <div class='file-info-element'>${this.lastModified}</div>
+      </div>
+    </div>
+    <hr>
+    `;
+    return obj;
+  }
+
+}
+
+export default FileListObject;
